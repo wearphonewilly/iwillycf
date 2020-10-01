75273004971f8495c2e7faf40798c0a5adababfe
Willy WearPhone <willy.cardil.wc@gmail.com>
2020-10-01 23:40:06 +0200

    Updating all

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
     'use strict';
     
     var $searchView = $('.menu-search-container');
-    var $menu = $('.menu-mac, .menu-iPad, .menu-iPhone, .menu-watch, .menu-tv, .menu-support, .menu-search, .menu-store');
+    var $menu = $('.menu-inicio, .menu-team, .menu-rutas, .menu-entrenos, .menu-eventos, .menu-contact, .menu-search, .menu-store');
     var $fadeScreen = $('.fade-screen');
     
     $('li.menu-search a, .fade-screen, .menu-search-close').on('click', function(e) {
